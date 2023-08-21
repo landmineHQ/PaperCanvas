b30b71bf583964ac3418b4cb3df34a28fe7b1406
landmineHQ <z1216198007@gmail.com>
2023-08-21 18:41:36 +0800

    [Feature] chain

---
+++ src/components/canvasCursor/interface/interface.ts
@@ -0,0 +1,78 @@
+interface Cursor {
+    // 鼠标点
+    point: paper.Point | undefined
+    // 激活
+    active: boolean
+    // 设置
+    config: {
+        size: {
+            point: SizeRangeOptions
+            circle: SizeRangeOptions
+        }
+        speed: {
+            point: SpeedOptions
+            circle: SpeedOptions
+        }
+    }
+}
+
+interface SizeRangeOptions {
+    normal: number
+    max: number
+    min: number
+}
+
+interface SpeedOptions {
+    in: number
+    out: number
+}
+
+const cursor: Cursor = {
+    point: undefined,
+    active: false,
+    config: {
+        size: {
+            circle: {
+                normal: 30,
+                min: 10,
+                max: 35
+            },
+            point: {
+                normal: 3,
+                min: 5,
+                max: 1
+            }
+        },
+        speed: {
+            circle: {
+                in: 0.3,
+                out: 0.1
+            },
+            point: {
+                in: 0.3,
+                out: 0.1
+            }
+        }
+    }
+}
+
+const chain: { path: paper.Path | null, [key: string]: any } = {
+    points: 45,
+    length: 25,
+    path: null,
+}
+
+interface EventHandler {
+    initListener: any
+    onMove: any
+    onDown: any
+    onUp: any
+    [key: string]: any
+}
+
+export {
+    type Cursor,
+    type EventHandler,
+    cursor,
+    chain,
+}
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( {
   plugins: [
     vue(),
     vueJsx(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath( new URL( './src', import.meta.url ) ),
+      '#': fileURLToPath( new URL( './', import.meta.url ) )
     }
   }
-})
+} )
