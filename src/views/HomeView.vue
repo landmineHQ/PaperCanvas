d2a12af349a74c3e9053326c9b1788aa1e507a3e
landmineHQ <z1216198007@gmail.com>
2023-08-21 20:46:07 +0800

    [Update] Home视图适配canvasCursor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
 import CanvasCursor from "@/components/canvasCursor/CanvasCursor.vue"
+import { ref } from "vue";
+const showChain = ref(true)
 </script>
 
 <template>
-  <CanvasCursor>
-    <div slot:default>
-      <h1>hello world</h1>
+  <CanvasCursor :show-chains="showChain">
+    <div slot:default class="container">
+      <h1>canvas cursor</h1>
+      <label for="id_showChain" id="toggleLabel">
+        <input type="checkbox" id="id_showChain" v-model="showChain"><em>toggle showChain</em>
+      </label>
     </div>
   </CanvasCursor>
 </template>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
 h1 {
   text-align: center;
   text-transform: uppercase;
   user-select: none;
+}
+
+#toggleLabel {
+  margin: 0 auto;
+  user-select: none;
+  cursor: none;
 }
 </style>
