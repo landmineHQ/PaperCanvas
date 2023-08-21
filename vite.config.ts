e8d54cb8fbf2988ea38644162268f93b78d676a9
landmineHQ <z1216198007@gmail.com>
2023-08-21 21:35:39 +0800

    [Fixed] base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig( {
+export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath( new URL( './src', import.meta.url ) ),
-      '#': fileURLToPath( new URL( './', import.meta.url ) )
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '#': fileURLToPath(new URL('./', import.meta.url))
     }
-  }
-} )
+  },
+  base: './'
+})
