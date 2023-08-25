6d6cdfa8a1f7f3e1397c45303f7735796a6f6f32
landmineHQ <z1216198007@gmail.com>
2023-08-25 22:05:24 +0800

    [Update] view BooleanOperations 增加配置选项

--- src/views/BooleanOperationsView.vue
+++ src/views/BooleanOperationsView.vue
@@ -1,30 +1,63 @@
 <template>
-    <BooleanOperations operation="exclude" fill-color="green">
-        <div class="container">
+    <BooleanOperations :operation="operation" :fill-color="color">
+        <div class="bo-container">
             <h1>boolean operations</h1>
-            <h3>布尔运算</h3>
+            <label class="select-opearation--box">
+                <h3>布尔运算</h3>
+                <select v-model="operation">
+                    <option v-for="item in operations" :value="item">{{ item }}</option>
+                </select>
+            </label>
+            <label>
+                <h3>颜色选择器</h3>
+                <input type="color" v-model="color">
+            </label>
         </div>
     </BooleanOperations>
 </template>
 
 <script setup lang="ts">
+import { ref, type Ref } from 'vue'
 import BooleanOperations from '@/components/booleanOperations/index.vue'
+import type { OperationsType } from '@/components/booleanOperations/interface/interface';
+import paperFull from 'paper';
+const operations = ['unite', 'intersect', 'subtract', 'exclude', 'divide', 'reorient']
+
+const operation: Ref<OperationsType> = ref("exclude")
+const color = ref(paperFull.Color.random().toCSS(true))
 </script>
 
 <style lang="scss" scoped>
-.container {
-    width: 100%;
-    height: 100%;
+.bo-container {
     position: relative;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
     user-select: none;
+    width: fit-content;
+    height: fit-content;
+    margin: 0 auto;
+    gap: 8px;
 }
 
 h1 {
     text-transform: uppercase;
-    user-select: none;
+}
+
+.select-opearation--box {
+
+    select {
+        width: 120px;
+        height: calc(100% - 4px);
+    }
+}
+
+h3 {
+    display: inline;
+
+    &::after {
+        content: "：";
+    }
 }
 </style>
